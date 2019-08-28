baca7c3949da9b9285bae20a102afd6f374b2afb
jesus portatil <jdevwd@gmail.com>
2019-08-28 18:16:16 +0200

    jgr-p - completo menu

--- mmcalculadora/Calculadora.java
+++ mmcalculadora/Calculadora.java
@@ -4,6 +4,10 @@ public class Calculadora {
 	
 	public static void menu() {
 		System.out.println("\nCALCULADORA\n");
+		System.out.println("sumar - s");
+		System.out.println("restar - r");
+		System.out.println("multiplicar - m");
+		System.out.println("dividir - d");
 	}
 
 	public static void main(String[] args) {
