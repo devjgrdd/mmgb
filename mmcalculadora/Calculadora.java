c95df0f01d175b7831402a879863e27c657e97f9
jesus portatil <jdevwd@gmail.com>
2019-08-28 19:40:47 +0200

    jgr-p - añado bucle y sumar()

--- mmcalculadora/Calculadora.java
+++ mmcalculadora/Calculadora.java
@@ -6,19 +6,43 @@ public class Calculadora {
 	
 	public static void menu() {
 		System.out.println("\nCALCULADORA\n");
-		System.out.println("sumar - s");
-		System.out.println("restar - r");
-		System.out.println("multiplicar - m");
-		System.out.println("dividir - d\n");
+		System.out.println("sumar - 1");
+		System.out.println("restar - 2");
+		System.out.println("multiplicar - 3");
+		System.out.println("dividir - 4\n");
+		System.out.println("finalizar - 0");
+	}
+	
+	public float sumar(float n1, float n2) {
+		return n1 + n2;
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		float n1 = 0.0f;
 		float n2 = 0.0f;
+		float res = 0.0f;
 		boolean continuar = true;
+		char opcion = ' ';
+		Calculadora calculadora = new Calculadora();
 		while(continuar) {
-			menu();
+			do {
+				menu();
+				opcion=sc.next().charAt(0);
+			}while(opcion<0 || opcion>4);
+			if(opcion==4) continuar = false;
+			else {
+				System.out.println("n1?");
+				n1=sc.nextFloat();
+				System.out.println("n2?");
+				n2=sc.nextFloat();
+				switch(opcion) {
+					case 1:
+						res = calculadora.sumar(n1,n2);
+						break;
+				}
+			}
+			System.out.println(res);
 		}
 		
 	}
