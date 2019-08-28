a5e90d20cdbb0214051847d5695ddec15597dfe8
jesus portatil <jdevwd@gmail.com>
2019-08-28 17:46:45 +0200

    jgr-p - menu

--- mmcalculadora/Calculadora.java
+++ mmcalculadora/Calculadora.java
@@ -1,9 +1,13 @@
 package mmcalculadora;
 
 public class Calculadora {
+	
+	public static void menu() {
+		System.out.println("\nCALCULADORA\n");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("HOLA MUNDO");
+		menu();
 	}
 
 }
